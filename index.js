0d4b95a79a6e033cc57924d738fae5ee5986563e
Emanuel Cortes Lugo <emanuelcodes@gmail.com>
2021-02-17 16:22:48 -0500

    Add lights with shadows.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 
@@ -19,12 +20,16 @@ const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 // Add img to obj
 const cubeMaterials = [
-  new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('img/1.png'), side: THREE.DoubleSide }), // LEFT
-  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/2.png'), side: THREE.DoubleSide }), // RIGHT
-  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/3.png'), side: THREE.DoubleSide }), // TOP
-  new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('img/4.png'), side: THREE.DoubleSide }), // BOTTOM
-  new THREE.MeshBasicMaterial({ color: 0x00ffff, side: THREE.DoubleSide }), // FRONT
-  // new THREE.MeshBasicMaterial({ color: 0xfff00f, side: THREE.DoubleSide }), // BACK
+  // new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('img/1.png'), side: THREE.DoubleSide }), // LEFT
+  // new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/2.png'), side: THREE.DoubleSide }), // RIGHT
+  // new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/3.png'), side: THREE.DoubleSide }), // TOP
+  // new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load('img/4.png'), side: THREE.DoubleSide }), // BOTTOM
+  new THREE.MeshLambertMaterial({ color: 0xFF5733, side: THREE.DoubleSide }), // FRONT
+  new THREE.MeshLambertMaterial({ color: 0x6495ED, side: THREE.DoubleSide }), // BACK
+  new THREE.MeshLambertMaterial({ color: 0xDFFF00, side: THREE.DoubleSide }),
+  new THREE.MeshLambertMaterial({ color: 0x40E0D0, side: THREE.DoubleSide }),
+  new THREE.MeshLambertMaterial({ color: 0xDE3163, side: THREE.DoubleSide }),
+
 ];
 
 // Shape
@@ -35,8 +40,19 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 // Scene lighting
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.0);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+// scene.add(ambientLight);
+
+const light1 = new THREE.PointLight(0xffffff, 4, 50);
+light1.position.set(10, 25, 25);
+light1.castShadow = true;
+scene.add(light1);
+
+const light2 = new THREE.PointLight(0xffffff, 4, 50);
+light2.position.set(-10, -25, 25);
+light2.castShadow = true;
+scene.add(light2);
+
 
 // Allows for the resizing of browser
 window.addEventListener('resize', function () {
@@ -47,7 +63,7 @@ window.addEventListener('resize', function () {
 
 const animate = function () {
   requestAnimationFrame(animate);
-  // cube.rotation.x += 0.01;
+  cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   // cube.rotation.z += 0.01;
 
