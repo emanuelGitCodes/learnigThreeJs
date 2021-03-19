8b84ab624f43df33d8f25ca865f86b748a9f3216
Emanuel Cortes Lugo <emanuelcodes@gmail.com>
2021-03-19 15:28:13 -0400

    My brain is burned out

--- sandBox.js
+++ sandBox.js
@@ -1,6 +1,7 @@
 function init() {
 	var scene = new THREE.Scene();
 	var gui = new dat.GUI();
+	var clock = new THREE.Clock();
 
 	var enableFog = false;
 
@@ -11,24 +12,68 @@ function init() {
 	var box = getBox(1, 1, 1);
 	var plane = getPlane(20);
 	var pointLight = getPointLight(1);
-	var sphere = getSphere(0.05);
+	var pointLightSphere = getSphere(0.05);
+	var spotLight = getSpotLight(1);
+	var spotLightSphere = getSphere(0.05);
+	var directionalLight = getDirectionalLight(1);
+	var directionalLightSphere = getSphere(0.05);
+	var ambientLight = getAmbientLight(0.5);
+	var ambientLightSphere = getSphere(0.05);
+
+	var helper = new THREE.CameraHelper(directionalLight.shadow.camera);
+
+	var boxGrid = getBoxGrid(7, 1.5);
 
 	plane.name = 'plane-1';
+	boxGrid.name = 'boxGrid';
 
 	box.position.y = box.geometry.parameters.height / 2;
 	plane.rotation.x = Math.PI / 2;
-	pointLight.position.set(0, 2, 0);
+
 	pointLight.intensity = 2;
+	pointLight.position.set(0, 2, 0);
+	spotLight.position.set(0, 4, 0);
+	directionalLight.position.set(0, 6, 0);
 
-	scene.add(box);
+	// scene.add(box);
 	scene.add(plane);
-	pointLight.add(sphere);
-	scene.add(pointLight);
 
-	gui.add(pointLight, 'intensity', 0, 10);
-	gui.add(pointLight.position, 'x', -5, 5);
-	gui.add(pointLight.position, 'y', 0, 5);
-	gui.add(pointLight.position, 'z', -5, 5);
+	pointLight.add(pointLightSphere);
+	// scene.add(pointLight);
+
+	spotLight.add(spotLightSphere);
+	// scene.add(spotLight);
+
+	directionalLight.add(directionalLightSphere);
+	scene.add(directionalLight);
+	scene.add(helper);
+
+	ambientLight.add(ambientLightSphere);
+	scene.add(ambientLight);
+
+	scene.add(boxGrid);
+
+	// gui.add(pointLight, 'intensity', 0, 10);
+	// gui.add(pointLight.position, 'x', -5, 5);
+	// gui.add(pointLight.position, 'y', 0, 5);
+	// gui.add(pointLight.position, 'z', -5, 5);
+
+	// gui.add(spotLight, 'intensity', 0, 10);
+	// // Penumbra is how hard the edge of the light is displayed.
+	// gui.add(spotLight, 'penumbra', 0, 1);
+	// gui.add(spotLight.position, 'x', 0, 20);
+	// gui.add(spotLight.position, 'y', 0, 20);
+	// gui.add(spotLight.position, 'z', 0, 20);
+
+	gui.add(directionalLight, 'intensity', 0, 10);
+	gui.add(directionalLight.position, 'x', -20, 20);
+	gui.add(directionalLight.position, 'y', 0, 20);
+	gui.add(directionalLight.position, 'z', -20, 20);
+
+	gui.add(ambientLight, 'intensity', 0, 10);
+	gui.add(ambientLight.position, 'x', -20, 20);
+	gui.add(ambientLight.position, 'y', 0, 20);
+	gui.add(ambientLight.position, 'z', -20, 20);
 
 	var camera = new THREE.PerspectiveCamera(
 		45,
@@ -37,7 +82,15 @@ function init() {
 		1000
 	);
 
-	camera.position.set(1, 2, 5);
+	// var camera = new THREE.OrthographicCamera(
+	// 	-15,
+	// 	15,
+	// 	15,
+	// 	-15,
+	// 	1,
+	// 	1000
+	// );
+	camera.position.set(20, 20, 25);
 	camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 	var renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -48,7 +101,7 @@ function init() {
 
 	var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-	update(renderer, scene, camera, controls);
+	update(renderer, scene, camera, controls, clock);
 
 	return scene;
 }
@@ -61,6 +114,33 @@ function getBox(w, h, d) {
 
 	mesh.castShadow = true;
 	return mesh;
+}
+
+// Create a grid of boxes of AREA = amount * amount
+function getBoxGrid(amount, separationMultiplier) {
+	var group = new THREE.Group();
+
+	for (var index = 0; index < amount; index++) {
+		var obj = getBox(1, 1, 1);
+		obj.position.x = index * separationMultiplier;
+		obj.position.y = obj.geometry.parameters.height / 2;
+		group.add(obj);
+
+		for (var cnt = 0; cnt < amount; cnt++) {
+			var obj = getBox(1, 1, 1);
+			obj.position.x = index * separationMultiplier;
+			obj.position.y = obj.geometry.parameters.height / 2;
+			obj.position.z = cnt * separationMultiplier;
+
+			group.add(obj);
+		}
+	}
+
+	group.position.x = -(separationMultiplier * (amount - 1)) / 2;
+	group.position.y = 0;
+	group.position.z = -(separationMultiplier * (amount - 1)) / 2;
+
+	return group;
 }
 
 function getPlane(size) {
@@ -89,8 +169,37 @@ function getPointLight(intensity) {
 	return light;
 }
 
+function getSpotLight(intensity) {
+	var light = new THREE.SpotLight(0xffffff, intensity);
+	light.castShadow = true;
+	// Treat the light.shadow.bias on a case by case bases.
+	light.shadow.bias = 0.001;
+	// shadow.mapSize width and height default value is 1024.
+	light.shadow.mapSize.width = 512;
+	light.shadow.mapSize.height = 512;
+	return light;
+}
+
+function getDirectionalLight(intensity) {
+	var light = new THREE.DirectionalLight(0xffffff, intensity);
+	light.castShadow = true;
+
+	light.shadow.camera.left = -10;
+	light.shadow.camera.bottom = -10;
+	light.shadow.camera.right = 10;
+	light.shadow.camera.top = 10;
+
+	return light;
+}
+
+function getAmbientLight(intensity) {
+	var light = new THREE.AmbientLight(0x000077, intensity);
+
+	return light;
+}
+
 // Updates the browser to allow for animation.
-function update(renderer, scene, camera, controls) {
+function update(renderer, scene, camera, controls, clock) {
 	renderer.render(
 		scene,
 		camera
@@ -98,8 +207,21 @@ function update(renderer, scene, camera, controls) {
 
 	controls.update();
 
+	var timeElapsed = clock.getElapsedTime();
+	var boxGrid = scene.getObjectByName('boxGrid');
+
+	boxGrid.children.forEach(function (child, index) {
+		var x = timeElapsed * 2 + index;
+		child.scale.y = (noise.simplex2(x, x) + 1) / 2 + 0.001;
+
+		// Get a random unique value between 0 and pi/2, to that number we add
+		//0.001 so it's not exactly flushed with the plane.
+		// child.scale.y = (Math.sin(timeElapsed * 2 + index) + 1) / 2 + 0.001;
+		child.position.y = child.scale.y / 2;
+	});
+
 	requestAnimationFrame(function () {
-		update(renderer, scene, camera, controls);
+		update(renderer, scene, camera, controls, clock);
 	});
 }
 
