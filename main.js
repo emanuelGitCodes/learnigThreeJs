895d0da91f96b5d42bc50a64de65eb444456c11e
Emanuel Cortes Lugo <emanuelcodes@gmail.com>
2021-03-19 10:24:40 -0400

    Add shadow to sandBox.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function init() {
 
 	box.position.y = box.geometry.parameters.height / 2;
 	plane.rotation.x = Math.PI / 2;
-	pointLight.position.set(0, 2, 0);
+	pointLight.position.y = 2;
 	pointLight.intensity = 2;
 
 	scene.add(box);
@@ -26,9 +26,7 @@ function init() {
 	scene.add(pointLight);
 
 	gui.add(pointLight, 'intensity', 0, 10);
-	gui.add(pointLight.position, 'x', -5, 5);
 	gui.add(pointLight.position, 'y', 0, 5);
-	gui.add(pointLight.position, 'z', -5, 5);
 
 	var camera = new THREE.PerspectiveCamera(
 		45,
@@ -37,13 +35,16 @@ function init() {
 		1000
 	);
 
-	camera.position.set(1, 2, 5);
+	camera.position.x = 1;
+	camera.position.y = 2;
+	camera.position.z = 5;
+
 	camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-	var renderer = new THREE.WebGLRenderer({ antialias: true });
+	var renderer = new THREE.WebGLRenderer();
 	renderer.shadowMap.enabled = true;
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setClearColor(0xbbbbbb);
+	renderer.setClearColor('rgb(120, 120, 120)');
 	document.getElementById('webgl').appendChild(renderer.domElement);
 
 	var controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -54,29 +55,42 @@ function init() {
 }
 
 function getBox(w, h, d) {
+	var geometry = new THREE.BoxGeometry(w, h, d);
+	var material = new THREE.MeshPhongMaterial({
+		color: 'rgb(120, 120, 120)'
+	});
 	var mesh = new THREE.Mesh(
-		new THREE.BoxGeometry(w, h, d),
-		new THREE.MeshPhongMaterial({ color: 0xbbbbbb, wireframe: false, }),
+		geometry,
+		material
 	);
-
 	mesh.castShadow = true;
+
 	return mesh;
 }
 
 function getPlane(size) {
+	var geometry = new THREE.PlaneGeometry(size, size);
+	var material = new THREE.MeshPhongMaterial({
+		color: 'rgb(120, 120, 120)',
+		side: THREE.DoubleSide
+	});
 	var mesh = new THREE.Mesh(
-		new THREE.PlaneGeometry(size, size),
-		new THREE.MeshPhongMaterial({ color: 0xbbbbbb, wireframe: false, side: THREE.DoubleSide, }),
+		geometry,
+		material
 	);
-
 	mesh.receiveShadow = true;
+
 	return mesh;
 }
 
-function getSphere(radius) {
+function getSphere(size) {
+	var geometry = new THREE.SphereGeometry(size, 24, 24);
+	var material = new THREE.MeshBasicMaterial({
+		color: 'rgb(255, 255, 255)'
+	});
 	var mesh = new THREE.Mesh(
-		new THREE.SphereGeometry(radius, 24, 24),
-		new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false, }),
+		geometry,
+		material
 	);
 
 	return mesh;
@@ -89,7 +103,6 @@ function getPointLight(intensity) {
 	return light;
 }
 
-// Updates the browser to allow for animation.
 function update(renderer, scene, camera, controls) {
 	renderer.render(
 		scene,
@@ -100,7 +113,7 @@ function update(renderer, scene, camera, controls) {
 
 	requestAnimationFrame(function () {
 		update(renderer, scene, camera, controls);
-	});
+	})
 }
 
 var scene = init();
